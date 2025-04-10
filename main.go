89e4382f4ce3203415cd65a564f9395855fb5e07
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-04-10 10:46:15 -0300

    updates

--- main.go
+++ main.go
@@ -6,13 +6,9 @@ import (
 	"placemaking-backend-go/routes"
 )
 
-// Handler é a única função exportada, usada pelo Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Inicializa Supabase antes de rodar as rotas
 	db.InitSupabase()
 
-	// Cria o router e serve a requisição
 	router := routes.SetupRouter()
 	router.ServeHTTP(w, r)
 }
-
